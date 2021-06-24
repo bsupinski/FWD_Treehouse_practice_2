19be02bb85c504ea9142455318f66c160c3cec06
Bsupinski <66806979+bsupinski@users.noreply.github.com>
2021-06-24 12:25:35 -0400

    Issue with label color

--- js/message-creator.js
+++ js/message-creator.js
@@ -1,3 +1,12 @@
+//Opening the message create menu
+const openMenu = document.querySelector(`.add-button`);
+const messageModal = document.querySelector('.create-message-overlay');
+
+openMenu.addEventListener("click", () =>{
+    messageModal.style.display = `flex`;
+})
+
+
 //Creating color option divs
 const labelContainer = document.querySelector('.label-choices');
 
@@ -29,4 +38,53 @@ styleColors.forEach(color => {
     colorDiv.className = "label-choices-options";
     labelContainer.appendChild(colorDiv);
 
+});
+
+//Getting user label choice
+var labelColor;
+
+labelContainer.addEventListener("click", (e)=>{
+    if(e.target !== labelContainer){
+        labelColor = getComputedStyle(e.target).backgroundColor;
+        console.log(labelColor)
+    }
+});
+
+
+
+
+//Getting Current date
+var todaysDate = new Date();
+const months = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+var today = `${months[todaysDate.getMonth()]} ${todaysDate.getDate()}, ${todaysDate.getFullYear()}`;
+
+
+//Posting message
+const messageBoard = document.querySelector('.messages');
+const postIt = document.querySelector('.post-it');
+var html;
+
+
+postIt.addEventListener("click", ()=>{
+    let yourTitle = document.getElementById('your-title').value;
+    let yourMessage = document.getElementById('your-message').value; 
+    html = 
+        `
+        <div class="new-message">
+            <div  class="date-wrapper">
+                <div style="background-color:${labelColor}; class="label-color"></div>
+                <div class="date">${today}</div>
+            </div>
+            <div class="message-wrapper">
+                <h4 class="title">${yourTitle}</h4>
+                <p class="message">${yourMessage}</p>
+            </div>
+        </div>
+        `;
+    
+
+    messageBoard.innerHTML+=html;
+    messageModal.style.display = `none`;
 });
